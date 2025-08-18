0bdb94c050fdea6e6412a25d12a45b5cb8ed6165
Pragnesh Shah <prgneshs@amazon.co.uk>
2025-08-18 22:10:32 +0000

    Replace landing_page.py

--- landing_page.py
+++ landing_page.py
@@ -1,6 +1,7 @@
 """
 This page provides information about core features of the demo.
 """
+
 import streamlit as st
 
 for key in st.session_state.keys():
@@ -8,13 +9,16 @@ for key in st.session_state.keys():
 
 # Title and Introduction
 st.title("Gen AI: Art of Possibility for AWS MAP Assessment")
-    # Content on the right side
+# Content on the right side
 st.markdown("""
     This demo illustrates the application of Generative AI during the MAP assessment phase, following the completion of on-premises discovery. It showcases capabilities that enhance migration planning, cost optimisation, identification of modernisation opportunities, and resource planning, processes which were previously both time-consuming and complex.
     - This demo can analyse infrastructure data to generate strategic recommendations, predict MAP funding milestones, and create comprehensive migration wave plans.
     - AWS partners can leverage these GenAI capabilities across three progressive implementation levels—from direct model usage to fully automated solutions—creating a transformative approach to cloud migration assessment.
     """)
-st.image("sampledata/landing_page_image.jpeg", caption="Generative AI in AWS MAP Assessment Phase")
+st.image(
+    "sampledata/landing_page_image.jpeg",
+    caption="Generative AI in AWS MAP Assessment Phase",
+)
 st.header("Key features:")
 st.markdown("""
 - **Modernisation Opportunity Analysis**: GenAI analyses architecture and on-premises infrastructure data to identify modernisation pathways with corresponding AWS cost projections.
@@ -24,6 +28,5 @@ st.markdown("""
 """)
 
 st.warning(
-   """ 💡 Remember, while our GenAI is a whiz at insights, it might occasionally +
-   daydream—so always double-check its predictions!"""
+    """ 💡 Remember, while our GenAI is a whiz at insights, it might occasionally daydream—so always double-check its predictions!"""
 )
